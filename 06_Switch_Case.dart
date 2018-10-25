0b570a0a096e9238057af64bffef3d40e3a20ede
Anshul Batra <31509682+anshulbatra@users.noreply.github.com>
2018-10-25 19:44:48 +0530

    Switch-case statements
    The switch statement evaluates an expression, matches the expression’s value to a case clause and executes the statements associated with that case.
    Switch Case Statements: Applicable for only 'int' and 'String'

---
+++ 06_Switch_Case.dart
@@ -0,0 +1,28 @@
+void main() {
+
+	// Switch Case Statements: Applicable for only 'int' and 'String'
+
+	String grade = 'A';
+
+	switch (grade) {
+
+		case 'A':
+			print("Grade A");
+			break;
+
+		case 'B':
+			print("Grade B");
+			break;
+
+		case 'C':
+			print("Grade C");
+			break;
+
+		case 'F':
+			print("Grade F");
+			break;
+      
+		default:
+			print("Invalid Grade");
+	}
+}
