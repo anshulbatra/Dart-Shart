539436b5ed77a5fb17a7c62bb53dc17add0939d6
Anshul Batra <31509682+anshulbatra@users.noreply.github.com>
2018-10-25 11:41:04 +0530

    Printing strings, numbers, variables, etc.

---
+++ 01_Hello_World.dart
@@ -0,0 +1,27 @@
+void main() {
+  
+  //This is a comment
+  print("Hello, World!");
+  
+  //Print strings using double quotes
+  print("I am Anshul Batra.");
+  
+  //Print numbers
+  print(3*6); 	//An integer value
+  print(4/8); 	//A double value
+  
+  //Print boolean
+  print(true);
+  
+  //Print using variables
+  var a = "Anshul";
+  var b = false;
+  var c = 5*6/89;
+  var d; 			  //An uninitialised variable
+  
+  print("My name is: $a");
+  print("Am I cool: $b");
+  print("This is a number: $c");
+  print("This is the result of printing an uninitialised variable: $d");
+  
+}
