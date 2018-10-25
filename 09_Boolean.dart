67620a1578cd318e4a98da87fc2896896ad68c6a
Anshul Batra <31509682+anshulbatra@users.noreply.github.com>
2018-10-26 02:13:27 +0530

    The Boolean data type
    The Boolean data type recognizes only the literal true as true.

---
+++ 09_Boolean.dart
@@ -0,0 +1,17 @@
+void main() { 
+
+   //DECLARATION
+   
+   bool test; 
+   test = 12 > 5; 
+   print(test); 
+   
+   //The Boolean data type recognizes only the literal true as true.
+   
+   var str = 'abc'; 
+   if(str) { 
+     print('String is not empty'); 
+    } else { 
+     print('Empty String'); 
+    } 
+}
