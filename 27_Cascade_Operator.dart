8f9400ae5d22d755bf23bec0d951aa2b4df966b9
Anshul Batra <31509682+anshulbatra@users.noreply.github.com>
2018-11-01 04:48:26 +0530

    Cascade operator to make series of invocations
    The above example invokes the methods in the class. However, every time a function is called, a reference to the object is required. The cascade operator can be used as a shorthand in cases where there is a sequence of invocations.
    
    The cascade ( .. ) operator can be used to issue a sequence of calls via an object.

---
+++ 27_Cascade_Operator.dart
@@ -0,0 +1,14 @@
+class Student { 
+   void test_method() { 
+      print("This is a  test method"); 
+   } 
+   
+   void test_method1() { 
+      print("This is a  test method1"); 
+   } 
+}  
+void main() { 
+   new Student() 
+   ..test_method() 
+   ..test_method1(); 
+}
